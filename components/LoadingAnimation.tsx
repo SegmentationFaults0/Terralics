ac8db6dab2bff24cc96b182419fb71483deb5ecc
Zeno Vandenbulcke <zeno.vandenbulcke@student.kuleuven.be>
2023-03-02 22:53:23 +0100

    start loading screen

--- components/LoadingAnimation.tsx
+++ components/LoadingAnimation.tsx
@@ -1,12 +1,17 @@
 import styles from "../styles/LoadingAnimation.module.css";
+import GlobeLogo from "../public/globe-logo.svg";
+import Image from "next/image";
 
 export default function LoadingAnimation() {
   return (
     <div className={styles.loadingBackground}>
-      <div className={styles.lds_ripple}>
-        <div></div>
-        <div></div>
-      </div>
+      <Image
+        src={GlobeLogo}
+        alt="lobe logo"
+        height={175}
+        width={175}
+        className={styles.logo}
+      />
     </div>
   );
 }
